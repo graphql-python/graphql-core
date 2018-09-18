59a0bd494a70f4420697ed43238b35db78b5cced
Syrus Akbary <me@syrusakbary.com>
2018-09-18 16:08:30 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,49 @@
 from re import search
 from setuptools import setup, find_packages
 
-with open('graphql/__init__.py') as init_file:
-    version = search("__version__ = '(.*)'", init_file.read()).group(1)
+with open("graphql/__init__.py") as init_file:
+    version = search('__version__ = "(.*)"', init_file.read()).group(1)
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='GraphQL-core-next',
+    name="GraphQL-core-next",
     version=version,
-
-    description='GraphQL-core-next is a Python port of GraphQL.js,'
-                ' the JavaScript reference implementation for GraphQL.',
+    description="GraphQL-core-next is a Python port of GraphQL.js,"
+    " the JavaScript reference implementation for GraphQL.",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    keywords='graphql',
-
-    url='https://github.com/graphql-python/graphql-core-next',
-
-    author='Christoph Zwerschke',
-    author_email='cito@online.de',
-    license='MIT license',
-
+    long_description_content_type="text/markdown",
+    keywords="graphql",
+    url="https://github.com/graphql-python/graphql-core-next",
+    author="Christoph Zwerschke",
+    author_email="cito@online.de",
+    license="MIT license",
     # PEP-561: https://www.python.org/dev/peps/pep-0561/
-    package_data={'graphql': ['py.typed']},
-
+    package_data={"graphql": ["py.typed"]},
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'],
-
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
     install_requires=[],
-    python_requires='>=3.6',
-    test_suite='tests',
+    python_requires=">=3.6",
+    test_suite="tests",
     tests_require=[
-        'pytest', 'pytest-asyncio', 'pytest-cov', 'pytest-describe',
-        'flake8', 'mypy', 'tox', 'python-coveralls'],
-    packages=find_packages(include=['graphql']),
+        "pytest",
+        "pytest-asyncio",
+        "pytest-cov",
+        "pytest-describe",
+        "flake8",
+        "mypy",
+        "tox",
+        "python-coveralls",
+    ],
+    packages=find_packages(include=["graphql"]),
     include_package_data=True,
-    zip_safe=False)
+    zip_safe=False,
+)
+
