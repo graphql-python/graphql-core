a5ba1da892d013da83099af8889156190cda44a2
Christoph Zwerschke <cito@online.de>
2019-03-08 21:25:56 +0100

    Merge schema validation tests with other schema tests
    Replicates graphql/graphql-js@8d7a5fd8013232d0f8664c7c431671a830f74329

--- tests/type/test_schema.py
+++ tests/type/test_schema.py
@@ -5,6 +5,7 @@ from graphql.type import (
     GraphQLField,
     GraphQLInterfaceType,
     GraphQLObjectType,
+    GraphQLScalarType,
     GraphQLSchema,
     GraphQLString,
     GraphQLInputObjectType,
@@ -63,6 +64,58 @@ def describe_type_system_schema():
                     GraphQLSchema(types={})
                 with raises(Exception):
                     GraphQLSchema(directives={})
+
+    def describe_a_schema_must_contain_uniquely_named_types():
+        def rejects_a_schema_which_redefines_a_built_in_type():
+            FakeString = GraphQLScalarType("String", serialize=lambda: None)
+
+            QueryType = GraphQLObjectType(
+                "Query",
+                {
+                    "normal": GraphQLField(GraphQLString),
+                    "fake": GraphQLField(FakeString),
+                },
+            )
+
+            with raises(TypeError) as exc_info:
+                GraphQLSchema(QueryType)
+            msg = str(exc_info.value)
+            assert msg == (
+                "Schema must contain unique named types"
+                f" but contains multiple types named 'String'."
+            )
+
+        def rejects_a_schema_which_defines_an_object_twice():
+            types = [
+                GraphQLObjectType("SameName", {}),
+                GraphQLObjectType("SameName", {}),
+            ]
+
+            with raises(TypeError) as exc_info:
+                GraphQLSchema(types=types)
+            msg = str(exc_info.value)
+            assert msg == (
+                "Schema must contain unique named types"
+                f" but contains multiple types named 'SameName'."
+            )
+
+        def rejects_a_schema_which_defines_fields_with_conflicting_types():
+            fields = {}
+            QueryType = GraphQLObjectType(
+                "Query",
+                {
+                    "a": GraphQLField(GraphQLObjectType("SameName", fields)),
+                    "b": GraphQLField(GraphQLObjectType("SameName", fields)),
+                },
+            )
+
+            with raises(TypeError) as exc_info:
+                GraphQLSchema(QueryType)
+            msg = str(exc_info.value)
+            assert msg == (
+                "Schema must contain unique named types"
+                f" but contains multiple types named 'SameName'."
+            )
 
         def describe_when_assumed_valid():
             def configures_the_schema_to_have_no_errors():
