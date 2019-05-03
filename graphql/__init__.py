1468a45e135acb59d7c32487ffbca4402cd3e231
Christoph Zwerschke <cito@online.de>
2019-05-04 00:44:03 +0200

    Prepare version 1.0.3

--- graphql/__init__.py
+++ graphql/__init__.py
@@ -37,7 +37,7 @@ The sub-packages of GraphQL-core-next are:
   - :mod:`graphql/subscription`: Subscribe to data updates.
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __version_js__ = "14.2.1"
 
 # The primary entry point into fulfilling a GraphQL request.
