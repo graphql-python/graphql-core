8f67b207b2d2d91c68e881cd2a8425851094166b
Christoph Zwerschke <cito@online.de>
2019-05-23 21:56:58 +0200

    Simplify an elif chain
    Partly replicates graphql/graphql-js@dec3cc5d5133118cdb19bb90a0a770c835497835

--- graphql/utilities/get_operation_root_type.py
+++ graphql/utilities/get_operation_root_type.py
@@ -24,17 +24,19 @@ def get_operation_root_type(
                 "Schema does not define the required query root type.", operation
             )
         return query_type
-    elif operation_type == OperationType.MUTATION:
+
+    if operation_type == OperationType.MUTATION:
         mutation_type = schema.mutation_type
         if not mutation_type:
             raise GraphQLError("Schema is not configured for mutations.", operation)
         return mutation_type
-    elif operation_type == OperationType.SUBSCRIPTION:
+
+    if operation_type == OperationType.SUBSCRIPTION:
         subscription_type = schema.subscription_type
         if not subscription_type:
             raise GraphQLError("Schema is not configured for subscriptions.", operation)
         return subscription_type
-    else:
-        raise GraphQLError(
-            "Can only have query, mutation and subscription operations.", operation
-        )
+
+    raise GraphQLError(
+        "Can only have query, mutation and subscription operations.", operation
+    )
