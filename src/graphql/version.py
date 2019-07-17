4b5d4823ff629421c09a5a303e970d973cee6f5c
Christoph Zwerschke <cito@online.de>
2019-07-17 15:31:28 +0200

    Improve version info and bump version serial

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -1,6 +1,15 @@
+import re
 from typing import NamedTuple
 
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
+
+
+version = "3.0.0a1"
+
+version_js = "14.4.0"
+
+
+_re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
 
 
 class VersionInfo(NamedTuple):
@@ -10,6 +19,23 @@ class VersionInfo(NamedTuple):
     releaselevel: str
     serial: int
 
+    @classmethod
+    def from_str(cls, v: str) -> "VersionInfo":
+        groups = _re_version.match(v).groups()  # type: ignore
+        major, minor, micro = map(int, groups[:3])
+        level = (groups[3] or "")[:1]
+        if level == "a":
+            level = "alpha"
+        elif level == "b":
+            level = "beta"
+        elif level in ("c", "r"):
+            level = "candidate"
+        else:
+            level = "final"
+        serial = groups[4]
+        serial = int(serial) if serial else 0
+        return cls(major, minor, micro, level, serial)
+
     def __str__(self):
         v = f"{self.major}.{self.minor}.{self.micro}"
         level = self.releaselevel
@@ -18,10 +44,6 @@ class VersionInfo(NamedTuple):
         return v
 
 
-version = "3.0.0a0"
+version_info = VersionInfo.from_str(version)
 
-version_info = VersionInfo(3, 0, 0, "alpha", 0)
-
-version_js = "14.4.0"
-
-version_info_js = VersionInfo(14, 4, 0, "final", 0)
+version_info_js = VersionInfo.from_str(version_js)
