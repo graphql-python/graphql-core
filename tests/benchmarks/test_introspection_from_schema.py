4f2d73d984bb025eec542713783e212bde611722
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2019-09-08 13:28:00 +0200

    Don't validate schema when setting up benchmark tests
    GraphQL.js does that too, and it shaves ~0.9s off the execution time:
    1.13s -> 0.69s for test_introspection_from_schema.py
    0.67s -> 0.21s for test_validate_gql.py

--- tests/benchmarks/test_introspection_from_schema.py
+++ tests/benchmarks/test_introspection_from_schema.py
@@ -6,7 +6,7 @@ from ..fixtures import big_schema_sdl  # noqa: F401
 
 
 def test_execute_introspection_query(benchmark, big_schema_sdl):  # noqa: F811
-    schema = build_schema(big_schema_sdl)
+    schema = build_schema(big_schema_sdl, assume_valid=True)
     query = parse(introspection_query.get_introspection_query())
     result = benchmark(lambda: execute(schema, query))
     assert result.errors is None
--- tests/benchmarks/test_validate_gql.py
+++ tests/benchmarks/test_validate_gql.py
@@ -6,7 +6,7 @@ from ..fixtures import big_schema_sdl  # noqa: F401
 
 
 def test_validate_introspection_query(benchmark, big_schema_sdl):  # noqa: F811
-    schema = build_schema(big_schema_sdl)
+    schema = build_schema(big_schema_sdl, assume_valid=True)
     query = parse(introspection_query.get_introspection_query())
     result = benchmark(lambda: validate(schema, query))
     assert result == []
