d0ad6a4310e37a3d6dc853d2930640053f89ae05
Christoph Zwerschke <cito@online.de>
2019-09-10 22:55:32 +0200

    Add docstring explaining how to run benchmarks

---
+++ tests/benchmarks/__init__.py
@@ -0,0 +1,9 @@
+"""Benchmarks for graphql
+
+Benchmarks are disabled (only executed as tests) by default in setup.cfg.
+You can enable them with --benchmark-enable if your want to execute them.
+
+E.g. in order to execute all the benchmarks with tox using Python 3.7::
+
+    tox -e py37 -- -k benchmarks --benchmark-enable
+"""
