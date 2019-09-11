e024793321742ff0fd0b7db9a0bbf966ba41f0ed
Christoph Zwerschke <cito@online.de>
2019-09-11 15:15:48 +0200

    Add benchmark for validating invalid query
    Replicates graphql/graphql-js@e591e953e970dab6398ae2f135c68f5ca380d729

---
+++ tests/benchmarks/test_validate_invalid_gql.py
@@ -0,0 +1,37 @@
+from graphql import build_schema, parse, validate
+
+# noinspection PyUnresolvedReferences
+from ..fixtures import big_schema_sdl  # noqa: F401
+
+
+def test_validate_invalid_query(benchmark, big_schema_sdl):  # noqa: F811
+    schema = build_schema(big_schema_sdl, assume_valid=True)
+    query_ast = parse(
+        """
+        {
+          unknownField
+          ... on unknownType {
+            anotherUnknownField
+            ...unknownFragment
+          }
+        }
+
+        fragment TestFragment on anotherUnknownType {
+          yetAnotherUnknownField
+        }
+        """
+    )
+    result = benchmark(lambda: validate(schema, query_ast))
+    assert result == [
+        {
+            "message": "Cannot query field 'unknownField' on type 'Query'.",
+            "locations": [(3, 11)],
+        },
+        {
+            "message": "Unknown type 'unknownType'. Did you mean 'UnknownSignature'?",
+            "locations": [(4, 18)],
+        },
+        {"message": "Unknown fragment 'unknownFragment'.", "locations": [(6, 16)]},
+        {"message": "Unknown type 'anotherUnknownType'.", "locations": [(10, 34)]},
+        {"message": "Fragment 'TestFragment' is never used.", "locations": [(10, 9)]},
+    ]
