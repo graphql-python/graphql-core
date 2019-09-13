c3f47da145bfc3d3e5ea96b9808ab8642e590859
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-09-13 22:15:41 +0100

    Added fix for is_nullish() that works with numpy arrays equality test (#60)

--- src/graphql/pyutils/is_nullish.py
+++ src/graphql/pyutils/is_nullish.py
@@ -1,3 +1,4 @@
+import math
 from typing import Any
 
 from ..error import INVALID
@@ -7,4 +8,8 @@ __all__ = ["is_nullish"]
 
 def is_nullish(value: Any) -> bool:
     """Return true if a value is null, undefined, or NaN."""
-    return value is None or value is INVALID or value != value
+    return (
+        value is None
+        or value is INVALID
+        or (isinstance(value, float) and math.isnan(value))
+    )
