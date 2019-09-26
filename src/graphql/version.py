0fb5b81fc0a1909bfe63df31657bc4de631676cf
Christoph Zwerschke <cito@online.de>
2019-09-26 13:47:30 +0200

    Version 3.0.0b1 is up-to-date with GraphQL.js 14.5.6

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.0.0b0"
+version = "3.0.0b1"
 
-version_js = "14.5.0"
+version_js = "14.5.6"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
