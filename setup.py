0368eef6504d2cb9895ac2bb00b3ff108427d161
Christoph Zwerschke <cito@online.de>
2019-12-30 11:38:19 +0100

    Python 3.8 was missing in setup.py
    PyYAML should not be a direct dependency.
    Also, update poetry lock file.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=[],
     python_requires=">=3.6,<4",
