37437abadb626cced2e8743a3ba64f343afe4aab
Christoph Zwerschke <cito@online.de>
2019-12-30 13:27:38 +0100

    Use cached_property from standard lib if possible

--- src/graphql/pyutils/cached_property.py
+++ src/graphql/pyutils/cached_property.py
@@ -1,24 +1,35 @@
-# Code taken from https://github.com/bottlepy/bottle
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    standard_cached_property = None
+else:
+    try:
+        from functools import cached_property as standard_cached_property
+    except ImportError:  # Python < 3.8
+        standard_cached_property = None
+
+if standard_cached_property:
+    cached_property = standard_cached_property
+else:
+    # Code taken from https://github.com/bottlepy/bottle
+
+    class CachedProperty:
+        """A cached property.
+
+        A property that is only computed once per instance and then replaces itself with
+        an ordinary attribute. Deleting the attribute resets the property.
+        """
+
+        def __init__(self, func):
+            self.__doc__ = getattr(func, "__doc__")
+            self.func = func
+
+        def __get__(self, obj, cls):
+            if obj is None:
+                return self
+            value = obj.__dict__[self.func.__name__] = self.func(obj)
+            return value
+
+    cached_property = CachedProperty
 
 __all__ = ["cached_property"]
-
-
-class CachedProperty:
-    """A cached property.
-
-    A property that is only computed once per instance and then replaces itself with an
-    ordinary attribute. Deleting the attribute resets the property.
-    """
-
-    def __init__(self, func):
-        self.__doc__ = getattr(func, "__doc__")
-        self.func = func
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        value = obj.__dict__[self.func.__name__] = self.func(obj)
-        return value
-
-
-cached_property = CachedProperty
