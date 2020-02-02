990b73a575096f7302e7ffa358eb7fdbc60e8137
Christoph Zwerschke <cito@online.de>
2020-02-02 18:23:55 +0100

    master is now up to date with GraphQL.js 15.0.0a2

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -6,7 +6,7 @@ __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 version = "3.1.0a0"
 
-version_js = "15.0.0a1"
+version_js = "15.0.0a2"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
