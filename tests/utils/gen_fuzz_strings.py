d32fd8fa8cd4a6d24e1881d049f062205eac5bb0
Christoph Zwerschke <cito@online.de>
2020-07-02 23:40:21 +0200

    Simplify gen_fuzz_strings using itertools

--- tests/utils/gen_fuzz_strings.py
+++ tests/utils/gen_fuzz_strings.py
@@ -1,3 +1,4 @@
+from itertools import product
 from typing import Generator
 
 __all__ = ["gen_fuzz_strings"]
@@ -5,20 +6,5 @@ __all__ = ["gen_fuzz_strings"]
 
 def gen_fuzz_strings(allowed_chars: str, max_length: int) -> Generator[str, None, None]:
     """Generator that produces all possible combinations of allowed characters."""
-    num_allowed_chars = len(allowed_chars)
-
-    num_combinations = 0
-    for length in range(1, max_length + 1):
-        num_combinations += num_allowed_chars ** length
-
-    yield ""  # special case for empty string
-    for combination in range(num_combinations):
-        permutation = ""
-
-        left_over = combination
-        while left_over >= 0:
-            reminder = left_over % num_allowed_chars
-            permutation = allowed_chars[reminder] + permutation
-            left_over = (left_over - reminder) // num_allowed_chars - 1
-
-        yield permutation
+    for length in range(max_length + 1):
+        yield from map("".join, product(allowed_chars, repeat=length))
