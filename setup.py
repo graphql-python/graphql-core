597fc283aefdaa2b03c6bc04f9196029e57ff8d6
Christoph Zwerschke <cito@online.de>
2021-02-06 19:55:23 +0100

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     install_requires=[],
     python_requires=">=3.6,<4",
