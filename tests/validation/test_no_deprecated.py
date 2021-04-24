8845f386dcd29fe546c2c609d05bca3e77cf4abc
Christoph Zwerschke <cito@online.de>
2021-04-24 22:04:01 +0200

    test_no_deprecated: simplify test cases
    Replicates graphql/graphql-js@19f2c5255280f78874559cab636f73838a01339e

--- tests/validation/test_no_deprecated.py
+++ tests/validation/test_no_deprecated.py
@@ -1,119 +1,155 @@
 from functools import partial
+from typing import Callable, List, Tuple
 
 from graphql.utilities import build_schema
 from graphql.validation import NoDeprecatedCustomRule
 
 from .harness import assert_validation_errors
 
-schema = build_schema(
-    """
-    enum EnumType {
-      NORMAL_VALUE
-      DEPRECATED_VALUE @deprecated(reason: "Some enum reason.")
-      DEPRECATED_VALUE_WITH_NO_REASON @deprecated
-    }
 
-    type Query {
-      normalField(enumArg: [EnumType]): String
-      deprecatedField: String @deprecated(reason: "Some field reason.")
-      deprecatedFieldWithNoReason: String @deprecated
-    }
-    """
-)
-
-assert_errors = partial(assert_validation_errors, NoDeprecatedCustomRule, schema=schema)
-
-assert_valid = partial(assert_errors, errors=[])
+def build_assertions(
+    sdl_str: str,
+) -> Tuple[Callable[[str], None], Callable[[str, List], None]]:
+    schema = build_schema(sdl_str)
+    assert_errors = partial(
+        assert_validation_errors, NoDeprecatedCustomRule, schema=schema
+    )
+    assert_valid = partial(assert_errors, errors=[])
+    return (
+        assert_valid,
+        assert_errors,
+    )  # type: ignore
 
 
 def describe_validate_no_deprecated():
-    def ignores_fields_and_enum_values_that_are_not_deprecated():
-        assert_valid(
+    def describe_no_deprecated_fields():
+        _assert_valid, _assert_errors = build_assertions(
             """
-            {
-              normalField(enumArg: [NORMAL_VALUE])
+            type Query {
+              normalField: String
+              deprecatedField: String @deprecated(reason: "Some field reason.")
             }
             """
         )
 
-    def ignores_unknown_fields_and_enum_values():
-        assert_valid(
+        def ignores_fields_and_enum_values_that_are_not_deprecated():
+            _assert_valid(
+                """
+                {
+                  normalField
+                }
+                """
+            )
+
+        def ignores_unknown_fields():
+            _assert_valid(
+                """
+                {
+                  unknownField
+                }
+
+                fragment UnknownFragment on UnknownType {
+                  unknownField
+                }
+                """
+            )
+
+        def reports_error_when_a_deprecated_field_is_selected():
+            message = (
+                "The field Query.deprecatedField is deprecated. Some field reason."
+            )
+            _assert_errors(
+                """
+                {
+                  deprecatedField
+                }
+
+                fragment QueryFragment on Query {
+                  deprecatedField
+                }
+                """,
+                [
+                    {
+                        "message": message,
+                        "locations": [(3, 19)],
+                    },
+                    {
+                        "message": message,
+                        "locations": [(7, 19)],
+                    },
+                ],
+            )
+
+    def describe_no_deprecated_enum_values():
+        _assert_valid, _assert_errors = build_assertions(
             """
-            fragment UnknownFragment on UnknownType {
-              unknownField(unknownArg: UNKNOWN_VALUE)
+            enum EnumType {
+              NORMAL_VALUE
+              DEPRECATED_VALUE @deprecated(reason: "Some enum reason.")
             }
 
-            fragment QueryFragment on Query {
-              unknownField(unknownArg: UNKNOWN_VALUE)
-              normalField(enumArg: UNKNOWN_VALUE)
+            type Query {
+              someField(enumArg: EnumType): String
             }
             """
         )
 
-    def reports_error_when_a_deprecated_field_is_selected():
-        assert_errors(
-            """
-            {
-              normalField
-              deprecatedField
-              deprecatedFieldWithNoReason
-            }
-            """,
-            [
+        def ignores_enum_values_that_are_not_deprecated():
+            _assert_valid(
+                """
                 {
-                    "message": "The field Query.deprecatedField is deprecated."
-                    " Some field reason.",
-                    "locations": [(4, 15)],
-                },
-                {
-                    "message": "The field Query.deprecatedFieldWithNoReason"
-                    " is deprecated. No longer supported",
-                    "locations": [(5, 15)],
-                },
-            ],
-        )
+                  normalField(enumArg: NORMAL_VALUE)
+                }
+                """
+            )
 
-    def reports_error_when_a_deprecated_enum_value_is_used():
-        assert_errors(
-            """
-            {
-              normalField(enumArg: [NORMAL_VALUE, DEPRECATED_VALUE])
-              normalField(enumArg: [DEPRECATED_VALUE_WITH_NO_REASON])
-            }
-            """,
-            [
-                {
-                    "message": "The enum value 'EnumType.DEPRECATED_VALUE'"
-                    " is deprecated. Some enum reason.",
-                    "locations": [(3, 51)],
-                },
-                {
-                    "message": "The enum value"
-                    " 'EnumType.DEPRECATED_VALUE_WITH_NO_REASON'"
-                    " is deprecated. No longer supported",
-                    "locations": [(4, 37)],
-                },
-            ],
-        )
+        def ignores_unknown_enum_values():
+            _assert_valid(
+                """
+                query (
+                  $unknownValue: EnumType = UNKNOWN_VALUE
+                  $unknownType: UnknownType = UNKNOWN_VALUE
+                ) {
+                  someField(enumArg: UNKNOWN_VALUE)
+                  someField(unknownArg: UNKNOWN_VALUE)
+                  unknownField(unknownArg: UNKNOWN_VALUE)
+                }
 
-    def reports_error_when_deprecated_field_or_enum_value_is_used_inside_a_fragment():
-        assert_errors(
-            """
-            fragment QueryFragment on Query {
-              deprecatedField
-              normalField(enumArg: [NORMAL_VALUE, DEPRECATED_VALUE])
-            }
-            """,
-            [
-                {
-                    "message": "The field Query.deprecatedField is deprecated."
-                    " Some field reason.",
-                    "locations": [(3, 15)],
-                },
-                {
-                    "message": "The enum value 'EnumType.DEPRECATED_VALUE'"
-                    " is deprecated. Some enum reason.",
-                    "locations": [(4, 51)],
-                },
-            ],
-        )
+                fragment SomeFragment on Query {
+                  someField(enumArg: UNKNOWN_VALUE)
+                }
+                """
+            )
+
+        def reports_error_when_a_deprecated_enum_value_is_used():
+            message = (
+                "The enum value 'EnumType.DEPRECATED_VALUE' is deprecated."
+                " Some enum reason."
+            )
+            _assert_errors(
+                """
+                query (
+                  $variable: EnumType = DEPRECATED_VALUE
+                ) {
+                  someField(enumArg: DEPRECATED_VALUE)
+                }
+
+                fragment QueryFragment on Query {
+                  someField(enumArg: DEPRECATED_VALUE)
+                }
+                """,
+                [
+                    {
+                        "message": message,
+                        "locations": [(3, 41)],
+                    },
+                    {
+                        "message": message,
+                        "locations": [(5, 38)],
+                    },
+                    {
+                        "message": message,
+                        "locations": [(9, 38)],
+                    },
+                ],
+            )
