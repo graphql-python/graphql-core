a3d3afca715fc1ff665d62fc58d177efd453cbf9
Christoph Zwerschke <cito@online.de>
2021-08-20 17:11:01 +0200

    Release v3.1.6, matching GraphQL.js v15.5.1

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.1.5"
+version = "3.1.6"
 
-version_js = "15.4.0"
+version_js = "15.5.1"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
