ac9c67425869ccac6eb038354055769f6df48311
Christoph Zwerschke <cito@online.de>
2021-09-29 12:19:35 +0200

    Fix camel to snake case conversion with digits (#140)
    Backport from main branch

--- tests/pyutils/test_convert_case.py
+++ tests/pyutils/test_convert_case.py
@@ -3,49 +3,54 @@ from graphql.pyutils import camel_to_snake, snake_to_camel
 
 def describe_camel_to_snake():
     def converts_typical_names():
-        result = camel_to_snake("CamelCase")
-        assert result == "camel_case"
-        result = camel_to_snake("InputObjectTypeExtensionNode")
-        assert result == "input_object_type_extension_node"
+        assert camel_to_snake("CamelCase") == "camel_case"
+        assert (
+            camel_to_snake("InputObjectTypeExtensionNode")
+            == "input_object_type_extension_node"
+        )
+        assert camel_to_snake("CamelToSnake") == "camel_to_snake"
 
     def may_start_with_lowercase():
-        result = camel_to_snake("CamelCase")
-        assert result == "camel_case"
+        assert camel_to_snake("camelCase") == "camel_case"
 
     def works_with_acronyms():
-        result = camel_to_snake("SlowXMLParser")
-        assert result == "slow_xml_parser"
-        result = camel_to_snake("FastGraphQLParser")
-        assert result == "fast_graph_ql_parser"
+        assert camel_to_snake("SlowXMLParser") == "slow_xml_parser"
+        assert camel_to_snake("FastGraphQLParser") == "fast_graph_ql_parser"
+
+    def works_with_numbers():
+        assert camel_to_snake("Python3Script") == "python3_script"
+        assert camel_to_snake("camel2snake") == "camel2snake"
 
     def keeps_already_snake():
-        result = camel_to_snake("snake_case")
-        assert result == "snake_case"
+        assert camel_to_snake("snake_case") == "snake_case"
 
 
 def describe_snake_to_camel():
     def converts_typical_names():
-        result = snake_to_camel("snake_case")
-        assert result == "SnakeCase"
-        result = snake_to_camel("input_object_type_extension_node")
-        assert result == "InputObjectTypeExtensionNode"
+        assert snake_to_camel("snake_case") == "SnakeCase"
+        assert (
+            snake_to_camel("input_object_type_extension_node")
+            == "InputObjectTypeExtensionNode"
+        )
+        assert snake_to_camel("snake_to_camel") == "SnakeToCamel"
 
     def may_start_with_uppercase():
-        result = snake_to_camel("Snake_case")
-        assert result == "SnakeCase"
+        assert snake_to_camel("Snake_case") == "SnakeCase"
 
     def works_with_acronyms():
-        result = snake_to_camel("slow_xml_parser")
-        assert result == "SlowXmlParser"
-        result = snake_to_camel("fast_graph_ql_parser")
-        assert result == "FastGraphQlParser"
+        assert snake_to_camel("slow_xml_parser") == "SlowXmlParser"
+        assert snake_to_camel("fast_graph_ql_parser") == "FastGraphQlParser"
+
+    def works_with_numbers():
+        assert snake_to_camel("python3_script") == "Python3Script"
+        assert snake_to_camel("snake2camel") == "Snake2camel"
 
     def keeps_already_camel():
-        result = snake_to_camel("CamelCase")
-        assert result == "CamelCase"
+        assert snake_to_camel("CamelCase") == "CamelCase"
 
     def can_produce_lower_camel_case():
-        result = snake_to_camel("snake_case", upper=False)
-        assert result == "snakeCase"
-        result = snake_to_camel("input_object_type_extension_node", False)
-        assert result == "inputObjectTypeExtensionNode"
+        assert snake_to_camel("snake_case", upper=False) == "snakeCase"
+        assert (
+            snake_to_camel("input_object_type_extension_node", False)
+            == "inputObjectTypeExtensionNode"
+        )
