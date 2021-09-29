ce98196bfed55c64aad5cf090d443e6bd5930e8c
Christoph Zwerschke <cito@online.de>
2021-09-29 16:15:01 +0200

    Tag as v3.2.0rc1, matching GraphQL.js v16.0.0rc1
    Replicates graphql/graphql-js@976d64b7633c5b3e1123ae3f657804907d7a4800

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.2.0a5"
+version = "3.2.0rc1"
 
-version_js = "16.0.0a5"
+version_js = "16.0.0rc1"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
