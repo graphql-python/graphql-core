91584ed3ce67a2d76402b1981282a12afc5f123f
Christoph Zwerschke <cito@online.de>
2021-12-26 13:45:44 +0100

    Do not use default name pattern for test classes
    Names like TestVisitor should not be interpreted as a class
    containing test methods (we use pytest_describe instead).
    Avoid such names in our test suite anyway.

--- tests/benchmarks/test_visit.py
+++ tests/benchmarks/test_visit.py
@@ -4,7 +4,7 @@ from graphql.language import visit, Visitor, ParallelVisitor
 from ..fixtures import big_schema_sdl  # noqa: F401
 
 
-class TestVisitor(Visitor):
+class DummyVisitor(Visitor):
     @staticmethod
     def enter(*args):
         pass
@@ -16,12 +16,12 @@ class TestVisitor(Visitor):
 
 def test_visit_all_ast_nodes(benchmark, big_schema_sdl):  # noqa: F811
     document_ast = parse(big_schema_sdl)
-    visitor = TestVisitor()
+    visitor = DummyVisitor()
     benchmark(lambda: visit(document_ast, visitor))
 
 
 def test_visit_all_ast_nodes_in_parallel(benchmark, big_schema_sdl):  # noqa: F811
     document_ast = parse(big_schema_sdl)
-    visitor = TestVisitor()
+    visitor = DummyVisitor()
     parallel_visitor = ParallelVisitor([visitor] * 50)
     benchmark(lambda: visit(document_ast, parallel_visitor))
