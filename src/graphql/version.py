acd613682495aa207b7054e0d8eaa45f82ae3b97
Christoph Zwerschke <cito@online.de>
2021-12-29 15:10:21 +0100

    Release as version 3.0.6

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.0.5"
+version = "3.0.6"
 
-version_js = "14.6.0"
+version_js = "14.7.0"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
