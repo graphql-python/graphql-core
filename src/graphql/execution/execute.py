ab26a0455bc21b1ec65d57667a0f14e682ccc302
Christoph Zwerschke <cito@online.de>
2022-02-03 22:42:14 +0100

    Remove unnecessary code in execute_fields_serially

--- src/graphql/execution/execute.py
+++ src/graphql/execution/execute.py
@@ -408,12 +408,6 @@ class ExecutionContext:
                 )
             else:
                 cast(Dict[str, Any], results)[response_name] = result
-        if is_awaitable(results):
-            # noinspection PyShadowingNames
-            async def get_results() -> Any:
-                return await cast(Awaitable, results)
-
-            return get_results()
         return results
 
     def execute_fields(
