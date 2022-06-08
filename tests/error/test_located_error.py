a5450f0298ddac8b89312d423e3ff7f8fc41dd94
Jonathan Kim <jkimbo@gmail.com>
2022-06-08 11:52:04 +0000

    Cast error message to string to handle proxy objects

--- tests/error/test_located_error.py
+++ tests/error/test_located_error.py
@@ -2,6 +2,8 @@ from typing import cast, Any
 
 from graphql.error import GraphQLError, located_error
 
+from ..utils import dedent
+
 
 def describe_located_error():
     def throws_without_an_original_error():
@@ -23,3 +25,24 @@ def describe_located_error():
         e = Exception("I am from elasticsearch")
         cast(Any, e).path = "/something/feed/_search"
         assert located_error(e, [], []) is not e
+
+    def handles_proxy_error_messages():
+        class ProxyString:
+            def __init__(self, value):
+                self.value = value
+
+            def __str__(self):
+                return self.value
+
+        class MyError(Exception):
+            def __init__(self):
+                self.message = ProxyString("Example error")
+                super().__init__()
+
+        error = located_error(MyError(), [], [])
+
+        assert str(error) == dedent(
+            """
+            Example error
+            """
+        )
