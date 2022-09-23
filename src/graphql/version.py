430ee891a52d239adadf133d873fdca2a3fe009b
Christoph Zwerschke <cito@online.de>
2022-09-23 10:16:58 +0200

    Release v3.2.3, matching GraphQL.js v16.6.0
    Replicates graphql/graphql-js@3a51ecade74a0198847e8b1ab1bcdc129485b79b

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.2.2"
+version = "3.2.3"
 
-version_js = "16.5.0"
+version_js = "16.6.0"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
