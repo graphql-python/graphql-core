e27ba48611f22638ce164baf02fe7524f96db32f
Christoph Zwerschke <cito@online.de>
2024-09-02 23:40:55 +0200

    Fix Sphinx warning

--- docs/conf.py
+++ docs/conf.py
@@ -145,6 +145,7 @@ types.TracebackType
 EnterLeaveVisitor
 FormattedSourceLocation
 GraphQLAbstractType
+GraphQLErrorExtensions
 GraphQLOutputType
 asyncio.events.AbstractEventLoop
 graphql.execution.map_async_iterator.MapAsyncIterator
