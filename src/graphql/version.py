91fbe867888709cf19d76d7397b6d9d85295d8fb
Christoph Zwerschke <cito@online.de>
2024-09-03 20:11:16 +0200

    Now matching GraphQL.js v16.7.1
    Replicates graphql/graphql-js@bf6a9f0e1cc8721de6675fb7bff470137635266f

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -6,7 +6,7 @@ __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 version = "3.2.3"
 
-version_js = "16.6.0"
+version_js = "16.7.1"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
