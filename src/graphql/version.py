dd4d5a1e89d86a149d3002a6bed2a47d9d5330a0
Christoph Zwerschke <cito@online.de>
2025-11-01 22:07:25 +0100

    Bump JavaScript version

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -6,7 +6,7 @@ __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 version = "3.2.7"
 
-version_js = "16.8.2"
+version_js = "16.9.0"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
