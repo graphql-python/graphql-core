51b874314bdcac0f503d845fe12f8d1ea8c44580
Christoph Zwerschke <cito@online.de>
2026-06-05 10:01:16 +0200

    Measure print/visit performance
    Note that the performance was already optimized in Python.
    
    Replicates graphql/graphql-js@31bf28f4

---
+++ tests/benchmarks/test_print.py
@@ -0,0 +1,9 @@
+from graphql import parse, print_ast
+
+from ..fixtures import kitchen_sink_query  # noqa: F401
+
+
+def test_print_kitchen_sink(benchmark, kitchen_sink_query):  # noqa: F811
+    document = parse(kitchen_sink_query)
+    result = benchmark(lambda: print_ast(document))
+    assert isinstance(result, str)
