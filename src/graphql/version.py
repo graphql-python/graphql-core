12bcb494e044e1cbe81b633ce5fadfa15af9874d
Christoph Zwerschke <cito@online.de>
2026-06-05 13:37:49 +0200

    Bump version
    Replicates graphql/graphql-js@7569989a8850c00a4e90ffdb56f1c6094cdcd795

--- src/graphql/version.py
+++ src/graphql/version.py
@@ -4,9 +4,9 @@ from typing import NamedTuple
 __all__ = ["version", "version_info", "version_js", "version_info_js"]
 
 
-version = "3.2.9"
+version = "3.2.10"
 
-version_js = "16.10.0"
+version_js = "16.13.0"
 
 
 _re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(\D*)(\d*)")
